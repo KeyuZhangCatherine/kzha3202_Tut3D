3acd3b85cba24bfe302f6fecb54dfc43f82115a6
Keyu Zhang <kzha3202@uni.sydney.edu.au>
2023-11-06 01:35:06 +1100

    Colours of blocks can be changed with audio and add rotation function
    Add createBlockWithAnimation to rotate the blocks. Use level and frequency, map the value and add into the block settings to change the colour of blocks with audio.

--- sketch.js
+++ sketch.js
@@ -137,45 +137,163 @@ function draw() {
   let level = amplitude.getLevel();
 
   //Map the amplitude level to a block size for the blue blocks
-  let blueBlockSize = map(level, 0, 1, 10, 100);
+  let blueBlockSize = map(level, 0, 1, 10, 150);
 
   //Analyze the frequency spectrum of the song
   let spectrum = fft.analyze();
 
   //Map the 50th frequency band to a block size for the red blocks
-  let redBlockSize = map(spectrum[50], 0, 255, 10, 100);
+  let redBlockSize = map(spectrum[50], 0, 255, 10, 150);
 
   //Map the 100th frequency band to a block size for the grey blocks
-  let greyBlockSize = map(spectrum[100], 0, 255, 10, 100); // Adjust the index (100) to suit your needs
+  let greyBlockSize = map(spectrum[100], 0, 255, 10, 150); 
 
-  //Pushe the current drawing style 
+  //Push the current drawing style 
   push();
   //Apply a translation transformation
   translate(0, 0);
 
-  //Create blue blocks with dynamic sizes based on the song amplitude level
-  createBlock(windowWidth * 0.1, windowHeight * 0.16, blueBlockSize, blueBlockSize, color(0, 0, 255));
-  createBlock(windowWidth * 0.1, windowHeight * 0.7, blueBlockSize, blueBlockSize, color(0, 0, 255));
-  createBlock(windowWidth * 0.32, windowHeight * 0.52, blueBlockSize, blueBlockSize, color(0, 0, 255));
-  createBlock(windowWidth * 0.76, windowHeight * 0.32, blueBlockSize, blueBlockSize, color(0, 0, 255));
-  createBlock(windowWidth * 0.82, windowHeight * 0.7, blueBlockSize, blueBlockSize, color(0, 0, 255));
+  //Create animated blue rectangles with varying positions and dynamic colors
+  createBlockWithAnimation(
+    windowWidth * 0.1, // X position
+    windowHeight * 0.16, // Y position
+    blueBlockSize, // Width of the rectangle
+    blueBlockSize, // Height of the rectangle
+    color(0, 0, map(blueBlockSize, 10, 100, 50, 255), 100) // Dynamic blue color with transparency
+  );
 
-  //Create red blocks with dynamic sizes based on a specific frequency band intensity
-  createBlock(windowWidth * 0.16, windowHeight * 0.04, redBlockSize, redBlockSize, color(255, 0, 0));
-  createBlock(windowWidth * 0.26, windowHeight * 0.04, redBlockSize, redBlockSize, color(255, 0, 0));
-  createBlock(windowWidth * 0.16, windowHeight * 0.54, redBlockSize, redBlockSize, color(255, 0, 0));
-  createBlock(windowWidth * 0.58, windowHeight * 0.4, redBlockSize, redBlockSize, color(255, 0, 0));
-  createBlock(windowWidth * 0.68, windowHeight * 0.6, redBlockSize, redBlockSize, color(255, 0, 0));
+  createBlockWithAnimation(
+    windowWidth * 0.1, // X position
+    windowHeight * 0.7, // Y position
+    blueBlockSize, // Width of the rectangle
+    blueBlockSize, // Height of the rectangle
+    color(0, 0, map(blueBlockSize, 10, 100, 50, 255), 100) // Dynamic blue color with transparency
+  );
 
-  //Create grey blocks with dynamic sizes based on a specific frequency band intensity
-  createBlock(windowWidth * 0.28, windowHeight * 0.08, greyBlockSize, greyBlockSize, color(169));
-  createBlock(windowWidth * 0.46, windowHeight * 0.22, greyBlockSize, greyBlockSize, color(169));
-  createBlock(windowWidth * 0.46, windowHeight * 0.74, greyBlockSize, greyBlockSize, color(169));
-  createBlock(windowWidth * 0.74, windowHeight * 0.62, greyBlockSize, greyBlockSize, color(169));
-  createBlock(windowWidth * 0.8, windowHeight * 0.04, greyBlockSize, greyBlockSize, color(169));
+  createBlockWithAnimation(
+    windowWidth * 0.32, // X position
+    windowHeight * 0.52, // Y position
+    blueBlockSize, // Width of the rectangle
+    blueBlockSize, // Height of the rectangle
+    color(0, 0, map(blueBlockSize, 10, 100, 50, 255), 100) // Dynamic blue color with transparency
+  );
+
+  createBlockWithAnimation(
+    windowWidth * 0.76, // X position
+    windowHeight * 0.32, // Y position
+    blueBlockSize, // Width of the rectangle
+    blueBlockSize, // Height of the rectangle
+    color(0, 0, map(blueBlockSize, 10, 100, 50, 255), 100) // Dynamic blue color with transparency
+  );
+
+  createBlockWithAnimation(
+    windowWidth * 0.82, // X position
+    windowHeight * 0.7, // Y position
+    blueBlockSize, // Width of the rectangle
+    blueBlockSize, // Height of the rectangle
+    color(0, 0, map(blueBlockSize, 10, 100, 50, 255), 100) // Dynamic blue color with transparency
+  );
+
+  // Create animated red rectangles with varying positions and dynamic colors
+  createBlockWithAnimation(
+    windowWidth * 0.16, // X position
+    windowHeight * 0.04, // Y position
+    redBlockSize, // Width of the rectangle
+    redBlockSize, // Height of the rectangle
+    color(map(redBlockSize, 10, 100, 255, 100), random(100), 0) // Dynamic red color 
+  );
+
+  createBlockWithAnimation(
+    windowWidth * 0.26, // X position
+    windowHeight * 0.04, // Y position
+    redBlockSize, // Width of the rectangle
+    redBlockSize, // Height of the rectangle
+    color(map(redBlockSize, 10, 100, 255, 100), 0, 0) // Dynamic red color 
+  );
+
+  createBlockWithAnimation(
+    windowWidth * 0.16, // X position
+    windowHeight * 0.54, // Y position
+    redBlockSize, // Width of the rectangle
+    redBlockSize, // Height of the rectangle
+    color(map(redBlockSize, 10, 100, 255, 100), 0, 0) // Dynamic red color 
+  );
+
+  createBlockWithAnimation(
+    windowWidth * 0.58, // X position
+    windowHeight * 0.4, // Y position
+    redBlockSize, // Width of the rectangle
+    redBlockSize, // Height of the rectangle
+    color(map(redBlockSize, 10, 100, 255, 100), 0, 0) // Dynamic red color 
+  );
+
+  createBlockWithAnimation(
+    windowWidth * 0.68, // X position
+    windowHeight * 0.6, // Y position
+    redBlockSize, // Width of the rectangle
+    redBlockSize, // Height of the rectangle
+    color(map(redBlockSize, 10, 100, 255, 100), 0, 0) // Dynamic red color 
+  );
+
+  // Create animated grey rectangles with varying positions and dynamic colors
+  createBlockWithAnimation(
+    windowWidth * 0.28, // X position
+    windowHeight * 0.08, // Y position
+    greyBlockSize, // Width of the rectangle
+    greyBlockSize, // Height of the rectangle
+    color(map(greyBlockSize, 10, 100, 100, 255), map(greyBlockSize, 10, 100, 255, 100), map(greyBlockSize, 10, 100, 255, 100)) // Dynamic grey color
+  );
+
+  createBlockWithAnimation(
+    windowWidth * 0.46, // X position
+    windowHeight * 0.22, // Y position
+    greyBlockSize, // Width of the rectangle
+    greyBlockSize, // Height of the rectangle
+    color(map(greyBlockSize, 10, 100, 100, 255), map(greyBlockSize, 10, 100, 255, 100), map(greyBlockSize, 10, 100, 255, 100)) // Dynamic grey color
+  );
+
+  createBlockWithAnimation(
+    windowWidth * 0.46, // X position
+    windowHeight * 0.74, // Y position
+    greyBlockSize, // Width of the rectangle
+    greyBlockSize, // Height of the rectangle
+    color(map(greyBlockSize, 10, 100, 100, 255), map(greyBlockSize, 10, 100, 255, 100), map(greyBlockSize, 10, 100, 255, 100)) // Dynamic grey color
+  );
+
+  createBlockWithAnimation(
+    windowWidth * 0.74, // X position
+    windowHeight * 0.62, // Y position
+    greyBlockSize, // Width of the rectangle
+    greyBlockSize, // Height of the rectangle
+    color(map(greyBlockSize, 10, 100, 100, 255), map(greyBlockSize, 10, 100, 255, 100), map(greyBlockSize, 10, 100, 255, 100)) // Dynamic grey color
+  );
+
+  createBlockWithAnimation(
+    windowWidth * 0.8, // X position
+    windowHeight * 0.04, // Y position
+    greyBlockSize, // Width of the rectangle
+    greyBlockSize, // Height of the rectangle
+    color(map(greyBlockSize, 10, 100, 100, 255), map(greyBlockSize, 10, 100, 255, 100), map(greyBlockSize, 10, 100, 255, 100)) // Dynamic grey color
+  );
 
   //Pop the current drawing style 
   pop();
+}
+
+//Define a function to create rotation blocks
+function createBlockWithAnimation(x, y, w, h, c) {
+  //Save the current drawing style
+  push(); 
+  //Move the origin of the drawing to the (x, y) position
+  translate(x, y);
+  //Rotate the drawing by an angle calculated from the number of frames displayed
+  rotate(radians(frameCount));
+  //Set the color used to fill shapes
+  fill(c);
+  //Draw a rectangle at the new origin
+  rect(0, 0, w, h);
+  //Restore the original drawing style settings and transformations
+  pop(); 
 }
 
 function mouseClicked() {
